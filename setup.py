69b70171e8a6cfe72d0272e3eff6c75ded37a78c
Yogesh Dhanapal <geoyogesh@gmail.com>
2019-01-25 16:27:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'flask',
-        'postal'
+        'flask'
     ],
     setup_requires=[
         'pytest-runner',
