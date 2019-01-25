81757489204e89d359a1e766206a4a3dd30073fb
Yogesh Dhanapal <geoyogesh@gmail.com>
2019-01-25 15:30:43 -0500

    for the input parameter for processing

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,7 +1,10 @@
 #!flask/bin/python
 import json
+from flask import request
 from flask import Flask, Response
 from helloworld.flaskrun import flaskrun
+#from postal.expand import expand_address
+#from postal.parser import parse_address
 
 application = Flask(__name__)
 
@@ -11,11 +14,21 @@ def get_hello_world():
 
 @application.route('/expand', methods=['GET'])
 def get_expand():
-    return Response(json.dumps({'Output': 'Hello World expand'}), mimetype='application/json', status=200)
+    # here we want to get the value of user (i.e. ?user=some-value)
+    user_input = request.args.get('q')
+    if (user_input):
+        return Response(json.dumps({'Output': 'please provide input query parameter q to complete expand request'}), mimetype='application/json', status=200)
+    else:
+        return Response(json.dumps({'Output': 'q=' + user_input}), mimetype='application/json', status=200)
 
 @application.route('/parser', methods=['GET'])
 def get_parser():
-    return Response(json.dumps({'Output': 'Hello World parser'}), mimetype='application/json', status=200)
+    # here we want to get the value of user (i.e. ?user=some-value)
+    user_input = request.args.get('q')
+    if (user_input):
+        return Response(json.dumps({'Output': 'please provide input query parameter q to comple parser request'}), mimetype='application/json', status=200)
+    else:
+        return Response(json.dumps({'Output': 'q=' + user_input}), mimetype='application/json', status=200)
 
 @application.route('/', methods=['POST'])
 def post_hollow_world():
