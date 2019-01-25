df328d875368054b721436ed90b0a044aee79af5
Yogesh Dhanapal <geoyogesh@gmail.com>
2019-01-25 15:20:19 -0500

    Update application.py

--- helloworld/application.py
+++ helloworld/application.py
@@ -6,11 +6,19 @@ from helloworld.flaskrun import flaskrun
 application = Flask(__name__)
 
 @application.route('/', methods=['GET'])
-def get():
+def get_hello_world():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
 
+@application.route('/expand', methods=['GET'])
+def get_expand():
+    return Response(json.dumps({'Output': 'Hello World expand'}), mimetype='application/json', status=200)
+
+@application.route('/parser', methods=['GET'])
+def get_parser():
+    return Response(json.dumps({'Output': 'Hello World parser'}), mimetype='application/json', status=200)
+
 @application.route('/', methods=['POST'])
-def post():
+def post_hollow_world():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
 
 if __name__ == '__main__':
